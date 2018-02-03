a5098f35aaa5e1d4fd6c37c9888496feca0c1012
DuWenLiang <739285514@qq.com>
2018-02-03 14:12:33 +0800

    Initial Commit

---
+++ NewsScrollbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsScrollbar
+//
+//  Created by 杜文亮 on 2018/2/3.
+//  Copyright © 2018年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsScrollbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsScrollbar
+//
+//  Created by 杜文亮 on 2018/2/3.
+//  Copyright © 2018年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
